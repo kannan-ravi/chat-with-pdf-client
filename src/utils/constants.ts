dcba50e38ffa220fe1f24c8f470a9e807187eb20
kannan-ravi <kannanravindran00kr@gmail.com>
2025-04-26 11:05:35 +0530

    Changes in environment variable for production

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,2 @@
-export const environment: string = "http://localhost:8000";
+// export const environment: string = "http://localhost:8000";
+export const environment: string = "https://api-chat-with-pdf.netlify.app/";
